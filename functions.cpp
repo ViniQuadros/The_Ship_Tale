23b1ddc00b5510461b23b2f9c92571e9b310d674
ViniQuadros <viniciusquadrosvq@gmail.com>
2023-08-07 21:05:14 -0300

    Adding Functions

--- functions.cpp
+++ functions.cpp
@@ -3,26 +3,42 @@
 #include <string>
 #include "functions.h"
 
-void Room()
+bool Room = true;
+bool Kitchen = true;
+
+std::vector <std::string> inventory;
+
+void Choose()
 {
-    bool room = true;
+    std::cout << "Choose: [1] Look,\n";
+    std::cout << "        [2] Take,\n";
+    std::cout << "        [3] Inventory,\n";
+}
 
-    std::vector <std::string> inventory;
-    std::vector <std::string> type;
-
-    std::string options;
-    std::cin >> options;
-
-    if(room == true && options == "Look"){
+void Look()
+{
+    if(Room == true){
         std::cout << "\nNow, you have the light to help you see.\n";
         std::cout << "Your room have only one bed, one small closet and a window showing the infinite sea.\n";
         std::cout << "Also, the lifeless brown of the walls make you feel sad\n";
         std::cout << "You can see the key of your room over the closet. It is old and rusty.\n";
     }
-    else if(room == true && options == "Take"){
+}
+
+void Take(){
+    if(Room == true){
         std::cout << "You took the key to your room\n";
         inventory.push_back("Room Key");
+        std::cout << "You have " << inventory[0];
     }
+}
 
-    std::cout << "You have " << inventory[0];
+void Inventory(){
+    if(inventory.empty()){
+        std::cout << "You have nothing with you!";
+    }
+    else{
+        for( unsigned int i = 0; i < inventory.size(); i++)
+            std::cout << i << std::endl;
+    }
 }
--- main.cpp
+++ main.cpp
@@ -8,9 +8,34 @@
 
 using namespace std;
 
+int writing;
+std::string mainQuestion = "\nWhat do you want to do?\n";
+
+void Type() {
+{
+    switch (writing)
+    {
+    case 1:
+        Look();
+        break;
+    case 2:
+        Take();
+        break;
+    case 3:
+        Inventory();
+        break;
+    default:
+        cout << "Invalid Option\n";
+        cout << mainQuestion;
+        cin >> writing;
+        Type();
+    }
+}
+}
+
 int main()
 {
-    int width = GetSystemMetrics(SM_CYSCREEN);
+    /*int width = GetSystemMetrics(SM_CYSCREEN);
     std::string name;
     char start;
     bool play = false;
@@ -19,14 +44,16 @@ int main()
     cout << setw(width / 3);
     cout << "*************** THE SHIP TALE ***************" << endl;
     cout << setw(width / 3);
-    cout << "   Press 1 to play the game or 2 to Quit    " << endl;
+    cout << Press 1 to "Look"; << endl;
+    cout << Press 2 to "Take"; << endl;
+    cout << "   Press p to play the game or q to Quit    " << endl;
     cin >> start;
     
-    if(start == '1')
+    if(start == 'p')
     {
         play = true;
     }
-    else if (start == '2'){
+    else if (start == 'q'){
         exit(1);
     }
     else{
@@ -63,7 +90,7 @@ int main()
         cout << "ARE YOU READY OR NOT??" << endl;
         cin >> Yes;
     }
-    } while (Yes != "yes" && Yes != "Yes" && Yes != "YES"); 
+    } while (Yes != "yes" && Yes != "Yes" && Yes != "YES");*/
 
     cout << "\nOk, then." << endl;
     cout << "\nYou wake up in your room, it is dark and you are all alone.";
@@ -71,10 +98,10 @@ int main()
     cout << "\nYou feel a little dizzy and decide to take a glass of water at the kitchen.";
     cout << "\nSo, you stand up from your unconfortable bed, turn the fire of the candle on and realize you went to sleep with your shoes.";
 
-    std::string mainQuestion = "\nWhat do you want to do?\n";
     cout << mainQuestion;
-
-    Room();
+    Choose();
+    cin >> writing;
+    Type();
 
     return 0;
 }
